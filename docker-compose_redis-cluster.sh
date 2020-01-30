d1e38e453436aab00376b9a1a89cf3cfcfdc3033
Haolun Zhang <chshawkn@users.noreply.github.com>
2020-01-30 16:13:38 +1300

    Valid cluster config for swarm and k8s (use host or pod IPs instead of container IPs)

---
+++ docker-compose_redis-cluster.sh
@@ -0,0 +1,10 @@
+
+docker-compose -f docker-compose_redis-cluster.yml down -v
+
+read -p "Do you want clear data: " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    find data/cluster ! -name '.gitignore' -type f -exec rm -rf {} +
+fi
+
+docker-compose -f docker-compose_redis-cluster.yml up -d
+docker-compose -f docker-compose_redis-cluster.yml logs -f
